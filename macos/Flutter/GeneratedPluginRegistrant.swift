f64ea0f6aa4936cf010021bcef647111eb0a2d99
Alexander Linder <seine@eloque.nz>
2025-04-18 13:05:50 +0200

    Update dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import file_selector_macos
 import package_info_plus
 import path_provider_foundation
 import share_plus
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import wakelock_plus
 
